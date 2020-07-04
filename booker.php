a434ca61a5ff4a5f2ba86db665f9ec90f62a41a6
MartinJ <mjoycemilburn@gmail.com>
2020-07-04 18:13:46 +0100

    Update booker.php

--- booker.php
+++ booker.php
@@ -1,5 +1,9 @@
 <?php
 
+// This is the recommended entry for the online Paypal booking operation mode of booker.html and the one that should be advertised to users. In the event 
+// that booker.html changes, the php call to booker.php will ensure the latest version is always used rather than out-of-date-cache.  The efficiency-loss 
+// can be disregarded in the circumstances
+
 include "booker.html";
 
 ?>
