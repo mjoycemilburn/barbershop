a7b3dd9cc3b65fb0ab26889c80d193bcb7d063f2
MartinJ <mjoycemilburn@gmail.com>
2020-07-13 15:27:14 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,4 +1,12 @@
 <?php
+
+# create header to pre-expire the page and make sure the browser reloads it.This ensures that 
+# php and html references also get reloaded if their version numbers have been changed
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +28,7 @@ password to apply credential checking to the operation of booker.html.
         <div>
             <script>
                 window.onload = function () {
-                    window.location.assign("booker.html?mode=viewer&ver=1.0");
+                    window.location.assign("booker.html?mode=viewer&ver=32.0");
                     };
              </script>       
         </div>
