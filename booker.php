ebb48d9264b43cae6082907a49c8b248355ded0c
MartinJ <mjoycemilburn@gmail.com>
2020-07-13 15:19:06 +0100

    Add files via upload

--- booker.php
+++ booker.php
@@ -1,11 +1,12 @@
 <?php
 
-// This is the recommended entry for the online Paypal booking operation mode of booker.html and the one that should be advertised to users. In the event 
-// that booker.html changes, the php call to booker.php will ensure the latest version is always used rather than out-of-date-cache.  The efficiency-loss 
-// can be disregarded in the circumstances
+# create header to pre-expire the page and make sure the browser reloads it.This ensures that 
+# php and html references also get reloaded if their version numbers have been changed
+header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 include "booker.html";
-
-?>
-
 
