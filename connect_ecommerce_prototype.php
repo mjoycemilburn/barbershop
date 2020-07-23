903eb5da3ed7516d9092003a1e3da2381039b205
MartinJ <mjoycemilburn@gmail.com>
2020-07-23 10:01:31 +0100

    Update connect_ecommerce_prototype.php

--- connect_ecommerce_prototype.php
+++ connect_ecommerce_prototype.php
@@ -1,10 +1,12 @@
 <?php
 
-# This routines need to be  copied manually to the home directories on the local PC (C:/home/qfgavcxt) and the Live server
+# This routine need to be  placed in the home directory of your server. It may seem a bit "over the top" to 
+# implement the connect/disconnect code as "includes" since, as presented here, they're exteremely simple. But
+# you may want to add other features such as logging, so payes to create smoe flexibility
+# 
 
 $con = mysqli_connect('hostname','root','password','database_name'); //**CONFIG REQUIRED**
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-?>
