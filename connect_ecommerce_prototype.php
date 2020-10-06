2e9992ca7f206770189cc1921265933c39a25da9
MartinJ <mjoycemilburn@gmail.com>
2020-10-06 11:32:58 +0100

    Update connect_ecommerce_prototype.php

--- connect_ecommerce_prototype.php
+++ connect_ecommerce_prototype.php
@@ -1,8 +1,6 @@
 <?php
 
-# This routine need to be  placed in the home directory of your server. It may seem a bit "over the top" to 
-# implement the connect/disconnect code as "includes" since, as presented here, they're exteremely simple. But
-# you may want to add other features such as logging, so payes to create smoe flexibility
+# This routine need to be  placed in the home directory of your server. 
 # 
 
 $con = mysqli_connect('hostname','root','password','database_name'); //**CONFIG REQUIRED**
